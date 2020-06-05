9b37b69aa0868603842c2d14e0073398c01a258d
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-06-05 16:32:45 -0400

    created client

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,6 +26,11 @@ namespace API {
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -40,6 +45,8 @@ namespace API {
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints => {
                 endpoints.MapControllers();
