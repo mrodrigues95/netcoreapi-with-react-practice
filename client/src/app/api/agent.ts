a973da950f8e9e7fd5d954c6bd4c1593b12aa1c5
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-06-07 20:30:23 -0400

    client side logging in

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,9 +1,11 @@
 import axios, { AxiosResponse } from 'axios';
 import { IActivity } from './../models/Activity';
 import { toast } from 'react-toastify';
+import { IUser, IUserFormValues } from './../models/User';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
+// Handle HTTP responses.
 axios.interceptors.response.use(undefined, (error) => {
   const { status, data, config } = error.response.status;
   if (error.message === 'Network Error' && !error.response) {
@@ -23,6 +25,7 @@ axios.interceptors.response.use(undefined, (error) => {
   if (status === 500) {
     toast.error('Server error - check terimnal for more info!');
   }
+  throw error.response;
 });
 
 const responseBody = (response: AxiosResponse) => response.data;
@@ -33,6 +36,7 @@ const sleep = (ms: number) => (response: AxiosResponse) =>
     setTimeout(() => resolve(response), ms)
   );
 
+// Handle requests.
 const requests = {
   get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
   post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
@@ -49,6 +53,13 @@ const Activities = {
   delete: (id: string) => requests.delete(`/activities/${id}`),
 };
 
+const User = {
+  current: (): Promise<IUser> => requests.get('/user'),
+  login: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/login`, user),
+  register: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/register`, user),
+}
+
 export default {
   Activities,
+  User
 };
